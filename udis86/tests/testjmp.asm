332e86d1ba4cadb6b18079c9a560baf9597a2b5a
Erdem U. Altinyurt <spamjunkeater@gmail.com>
2011-05-30 19:26:32 +0000

    Included libudis86 source to project.

---
+++ udis86/tests/testjmp.asm
@@ -0,0 +1,17 @@
+[bits 64]
+
+	jnz near x
+	jo near x
+	jno word x
+	jc near x
+	jnc word x
+	jae dword x
+	jcxz x
+	jecxz x
+	jrcxz x
+	jmp dword near x
+	call dword near x
+	jmp word x
+	jmp dword x
+	jmp word [eax]	
+x:	jmp qword [rax]

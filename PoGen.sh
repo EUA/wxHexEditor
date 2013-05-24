66f60788ae91aede59c1fcc06e7bff05b035defa
Erdem U. Altinyurt <spamjunkeater@gmail.com>
2013-05-24 09:36:54 +0000

    Added Po Generator script.

---
+++ PoGen.sh
@@ -0,0 +1,2 @@
+./GUILocaleFix.sh
+xgettext src/*.h src/*.cpp src/HexEditorCtrl/*.cpp src/HexEditorCtrl/*.h  src/HexEditorCtrl/wxHexCtrl/*.cpp src/HexEditorCtrl/wxHexCtrl/*.h -C -k_ -o locale/wxHexEditor.pot

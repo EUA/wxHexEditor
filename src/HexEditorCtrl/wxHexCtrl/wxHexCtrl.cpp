75b45a2bbde3bef2d6c4ddc00688f8bb6f2e2375
KaiN <kain@piwnica.ws>
2020-03-30 08:16:02 +0200

    fixed ambiguity warning

--- src/HexEditorCtrl/wxHexCtrl/wxHexCtrl.cpp
+++ src/HexEditorCtrl/wxHexCtrl/wxHexCtrl.cpp
@@ -564,7 +564,7 @@ inline wxDC* wxHexCtrl::UpdateDC(wxDC *xdc ) {
 					///rebuilding buffer from text
 					//First half of byte
 					RenderedHexByte += CharAt(textLenghtLimit++);
-					chr = RenderedHexByte.ToAscii()[0];
+					chr = RenderedHexByte.ToAscii().data()[0];
 					chrC = atoh( chr ) << 4;
 
 					//Space could be here deu custom formating
@@ -576,7 +576,7 @@ inline wxDC* wxHexCtrl::UpdateDC(wxDC *xdc ) {
 
 					//Second half of byte.
 					RenderedHexByte += CharAt(textLenghtLimit++);
-					chr = RenderedHexByte.ToAscii()[1];
+					chr = RenderedHexByte.ToAscii().data()[1];
 					chrC |= atoh( chr );
 					//chrC = (atoh( RenderedHexByte.ToAscii()[0] ) << 4) | atoh( RenderedHexByte.ToAscii()[1] );
 

e23a1b82c51e8c67a37e88da1f3a2eafc59dbaaf
KaiN <kain@piwnica.ws>
2020-03-30 08:14:15 +0200

    fixed redefinition warnings of HAVE_FSYNC macro

--- mhash/include/mutils/mhash_config.win32.h
+++ mhash/include/mutils/mhash_config.win32.h
@@ -2,149 +2,243 @@
 /* include/mutils/mhash_config.h.in.  Generated from configure.in by autoheader.  */
 
 /* "Adler32 Algorithm" */
+#ifndef ENABLE_ADLER32
 #define ENABLE_ADLER32 1
+#endif
 
 /* "CRC32 Algorithm" */
+#ifndef ENABLE_CRC32
 #define ENABLE_CRC32 1
+#endif
 
 /* "GOST Algorithm" */
+#ifndef ENABLE_GOST
 #define ENABLE_GOST 1
+#endif
 
 /* "Haval Algorithm" */
+#ifndef ENABLE_HAVAL
 #define ENABLE_HAVAL 1
+#endif
 
 /* "MD-2 Algorithm" */
+#ifndef ENABLE_MD2
 #define ENABLE_MD2 1
+#endif
 
 /* "MD-4 Algorithm" */
+#ifndef ENABLE_MD4
 #define ENABLE_MD4 1
+#endif
 
 /* "MD-5 Algorithm" */
+#ifndef ENABLE_MD5
 #define ENABLE_MD5 1
+#endif
 
 /* "RIPE-MD - RIPE Substitute for MD" */
+#ifndef ENABLE_RIPEMD
 #define ENABLE_RIPEMD 1
+#endif
 
 /* "SHA-1 Algorithm" */
+#ifndef ENABLE_SHA1
 #define ENABLE_SHA1 1
+#endif
 
 /* "SHA-224 and SHA-256 - SHA-2 Algorithms" */
+#ifndef ENABLE_SHA256_SHA224
 #define ENABLE_SHA256_SHA224 1
+#endif
 
 /* "SHA-384 and SHA-512 - SHA-2 Algorithms" */
+#ifndef ENABLE_SHA512_SHA384
 #define ENABLE_SHA512_SHA384 1
+#endif
 
 /* "Snefru Algorithm" */
+#ifndef ENABLE_SNEFRU
 #define ENABLE_SNEFRU 1
+#endif
 
 /* "Tiger Algorithm" */
+#ifndef ENABLE_TIGER
 #define ENABLE_TIGER 1
+#endif
 
 /* "Whirlpool Algorithm" */
+#ifndef ENABLE_WHIRLPOOL
 #define ENABLE_WHIRLPOOL 1
+#endif
 
 /* Define to 1 if you have the <assert.h> header file. */
+#ifndef HAVE_ASSERT_H
 #define HAVE_ASSERT_H 1
+#endif
 
 /* Define to 1 if you have the `bcopy' function. */
+#ifndef HAVE_BCOPY
 #define HAVE_BCOPY 1
+#endif
 
 /* Define to 1 if you have the <byteswap.h> header file. */
+#ifndef HAVE_BYTESWAP_H
 #define HAVE_BYTESWAP_H 1
+#endif
 
 /* Define to 1 if you have the `bzero' function. */
+#ifndef HAVE_BZERO
 #define HAVE_BZERO 1
+#endif
 
 /* Define to 1 if you have the <ctype.h> header file. */
+#ifndef HAVE_CTYPE_H
 #define HAVE_CTYPE_H 1
+#endif
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
+#ifndef HAVE_DLFCN_H
 #define HAVE_DLFCN_H 1
+#endif
 
 /* Define to 1 if you have the <endian.h> header file. */
+#ifndef HAVE_ENDIAN_H
 #define HAVE_ENDIAN_H 1
+#endif
 
 /* Define to 1 if you have the <errno.h> header file. */
+#ifndef HAVE_ERRNO_H
 #define HAVE_ERRNO_H 1
+#endif
 
 /* Define to 1 if you have the `fcntl' function. */
+#ifndef HAVE_FCNTL
 #define HAVE_FCNTL 1
+#endif
 
 /* Define to 1 if you have the <fcntl.h> header file. */
+#ifndef HAVE_FCNTL_H
 #define HAVE_FCNTL_H 1
+#endif
 
 /* Define to 1 if you have the `fsync' function. */
+#ifndef HAVE_FSYNC
 #define HAVE_FSYNC 1
+#endif
 
 /* Define to 1 if you have the <inttypes.h> header file. */
+#ifndef HAVE_INTTYPES_H
 #define HAVE_INTTYPES_H 1
+#endif
 
 /* Define to 1 if you have the `dmalloc' library (-ldmalloc). */
 /* #undef HAVE_LIBDMALLOC */
 
 /* Define to 1 if you have the <limits.h> header file. */
+#ifndef HAVE_LIMITS_H
 #define HAVE_LIMITS_H 1
+#endif
 
 /* Define to 1 if you have the `lstat' function. */
+#ifndef HAVE_LSTAT
 #define HAVE_LSTAT 1
+#endif
 
 /* Define to 1 if your system has a GNU libc compatible `malloc' function, and
    to 0 otherwise. */
+#ifndef HAVE_MALLOC
 #define HAVE_MALLOC 1
+#endif
 
 /* Define to 1 if you have the <malloc.h> header file. */
+#ifndef HAVE_MALLOC_H
 #define HAVE_MALLOC_H 1
+#endif
 
 /* Define to 1 if you have the `memmove' function. */
+#ifndef HAVE_MEMMOVE
 #define HAVE_MEMMOVE 1
+#endif
 
 /* Define to 1 if you have the <memory.h> header file. */
+#ifndef HAVE_MEMORY_H
 #define HAVE_MEMORY_H 1
+#endif
 
 /* Define to 1 if you have the `memset' function. */
+#ifndef HAVE_MEMSET
 #define HAVE_MEMSET 1
+#endif
 
 /* Define to 1 if you have the `mlock' function. */
+#ifndef HAVE_MLOCK
 #define HAVE_MLOCK 1
+#endif
 
 /* Define to 1 if you have the `readdir' function. */
+#ifndef HAVE_READDIR
 #define HAVE_READDIR 1
+#endif
 
 /* Define to 1 if you have the `readdir_r' function. */
+#ifndef HAVE_READDIR_R
 #define HAVE_READDIR_R 1
+#endif
 
 /* Define to 1 if you have the `sigaction' function. */
+#ifndef HAVE_SIGACTION
 #define HAVE_SIGACTION 1
+#endif
 
 /* Define to 1 if you have the `signal' function. */
+#ifndef HAVE_SIGNAL
 #define HAVE_SIGNAL 1
+#endif
 
 /* Define to 1 if you have the <signal.h> header file. */
+#ifndef HAVE_SIGNAL_H
 #define HAVE_SIGNAL_H 1
+#endif
 
 /* Define to 1 if you have the `stat' function. */
+#ifndef HAVE_STAT
 #define HAVE_STAT 1
+#endif
 
 /* Define to 1 if stdbool.h conforms to C99. */
+#ifndef HAVE_STDBOOL_H
 #define HAVE_STDBOOL_H 1
+#endif
 
 /* Define to 1 if you have the <stdint.h> header file. */
+#ifndef HAVE_STDINT_H
 #define HAVE_STDINT_H 1
+#endif
 
 /* Define to 1 if you have the <stdio.h> header file. */
+#ifndef HAVE_STDIO_H
 #define HAVE_STDIO_H 1
+#endif
 
 /* Define to 1 if you have the <stdlib.h> header file. */
+#ifndef HAVE_STDLIB_H
 #define HAVE_STDLIB_H 1
+#endif
 
 /* Define to 1 if you have the <strings.h> header file. */
+#ifndef HAVE_STRINGS_H
 #define HAVE_STRINGS_H 1
+#endif
 
 /* Define to 1 if you have the <string.h> header file. */
+#ifndef HAVE_STRING_H
 #define HAVE_STRING_H 1
+#endif
 
 /* Define to 1 if you have the `strtol' function. */
+#ifndef HAVE_STRTOL
 #define HAVE_STRTOL 1
+#endif
 
 /* Define to 1 if you have the <sys/endian.h> header file. */
 /* #undef HAVE_SYS_ENDIAN_H */
@@ -153,64 +247,98 @@
 /* #undef HAVE_SYS_MMAN_H 1 */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
+#ifndef HAVE_SYS_STAT_H
 #define HAVE_SYS_STAT_H 1
+#endif
 
 /* Define to 1 if you have the <sys/types.h> header file. */
+#ifndef HAVE_SYS_TYPES_H
 #define HAVE_SYS_TYPES_H 1
+#endif
 
 /* Define to 1 if you have the `umask' function. */
+#ifndef HAVE_UMASK
 #define HAVE_UMASK 1
+#endif
 
 /* Define to 1 if you have the <unistd.h> header file. */
+#ifndef HAVE_UNISTD_H
 #define HAVE_UNISTD_H 1
+#endif
 
 /* Define to 1 if you have the `utime' function. */
+#ifndef HAVE_UTIME
 #define HAVE_UTIME 1
+#endif
 
 /* Define to 1 if you have the <utime.h> header file. */
+#ifndef HAVE_UTIME_H
 #define HAVE_UTIME_H 1
+#endif
 
 /* Define to 1 if you have the <values.h> header file. */
 /* #undef HAVE_VALUES_H */
 
 /* Define to 1 if the system has the type `_Bool'. */
+#ifndef HAVE__BOOL
 #define HAVE__BOOL 1
+#endif
 
 /* "MHash Version" */
+#ifndef MHASH_VERSION
 #define MHASH_VERSION PROGRAM_VERSION
+#endif
 
 /* Define to 1 if your C compiler doesn't accept -c and -o together. */
 /* #undef NO_MINUS_C_MINUS_O */
 
 /* Name of package */
+#ifndef PACKAGE
 #define PACKAGE "mhash"
+#endif
 
 /* Define to the address where bug reports for this package should be sent. */
+#ifndef PACKAGE_BUGREPORT
 #define PACKAGE_BUGREPORT "mhash-devel@sourceforge.net"
+#endif
 
 /* Define to the full name of this package. */
+#ifndef PACKAGE_NAME
 #define PACKAGE_NAME "MHASH"
+#endif
 
 /* Define to the full name and version of this package. */
+#ifndef PACKAGE_STRING
 #define PACKAGE_STRING "MHASH 0.9.9"
+#endif
 
 /* Define to the one symbol short name of this package. */
+#ifndef PACKAGE_TARNAME
 #define PACKAGE_TARNAME "mhash"
+#endif
 
 /* Define to the version of this package. */
+#ifndef PACKAGE_VERSION
 #define PACKAGE_VERSION "0.9.9"
+#endif
 
 /* Define to 1 if the C compiler supports function prototypes. */
+#ifndef PROTOTYPES
 #define PROTOTYPES 1
+#endif
 
 /* Define to 1 if you have the ANSI C header files. */
+#ifndef STDC_HEADERS
 #define STDC_HEADERS 1
+#endif
 
 /* dmalloc */
 /* #undef USE_DMALLOC */
 
 /* Version number of package */
+#ifndef VERSION
 #define VERSION "0.9.9"
+#endif
 
 /* Define if using the dmalloc debugging malloc package */
 /* #undef WITH_DMALLOC */
@@ -242,7 +370,9 @@
 /* #undef _POSIX_SOURCE */
 
 /* Define like PROTOTYPES; this can be used by system headers. */
+#ifndef __PROTOTYPES
 #define __PROTOTYPES 1
+#endif
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
